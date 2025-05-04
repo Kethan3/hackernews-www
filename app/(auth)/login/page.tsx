c6fce51709157b8070833fe0f5f9c18e369965b8
Kethan3 <kethangowda003@gmail.com>
2025-05-04 20:00:48 +0530

    Remove unused useEffect import in LoginPage component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { betterAuthClient } from "@/lib/integrations/better-auth";
 import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 
 import Link from "next/link";
 import NavigationBar from "@/components/Navbar";
