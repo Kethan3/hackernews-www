bb3656f727ab5b54eb07cb9d07eb0ed91587912a
Kethan3 <kethangowda003@gmail.com>
2025-05-07 19:54:14 +0530

    Integrate CreatePost component into RootPage; display button only for logged-in users

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,14 @@
 // }
 
 // export default RootPage
-
 import React from "react";
 import PostList from "./pages/PostsLists";
+import CreatePost from "./pages/CreatePost";
+import { betterAuthClient } from "@/lib/integrations/better-auth";
 
 const RootPage = () => {
+  const { data: session } = betterAuthClient.useSession(); // Using betterAuthClient to get session data
+
   return (
     <div className="max-w-7xl mx-auto p-4 grid grid-cols-1 md:grid-cols-3 gap-6">
       {/* Posts List Section */}
@@ -22,11 +25,14 @@ const RootPage = () => {
       </div>
 
       {/* Create Post Button Section (Visible only if logged in) */}
-      <div className="md:col-span-1 flex justify-center items-center">
-        <PostList />
-      </div>
+      {session && (
+        <div className="md:col-span-1 flex justify-center items-center">
+          <CreatePost onPostCreated={() => {}} />
+        </div>
+      )}
     </div>
   );
 };
 
 export default RootPage;
+
